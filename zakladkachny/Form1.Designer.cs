ec008bcd42d87f437bd45a28c496b4ba044717e0
MartinGCHD <engstm@gchd.cz>
2018-01-09 16:05:02 +0100

    Moje Kachna

--- zakladkachny/Form1.Designer.cs
+++ zakladkachny/Form1.Designer.cs
@@ -29,27 +29,59 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Info = new System.Windows.Forms.Label();
+            this.GameControl = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // timer
             // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 50;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Info
+            // 
+            this.Info.AutoSize = true;
+            this.Info.Location = new System.Drawing.Point(665, 9);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(179, 13);
+            this.Info.TabIndex = 1;
+            this.Info.Text = "label1aaaaaaaaaaaaaaaaaaaaaaaa";
+            // 
+            // GameControl
+            // 
+            this.GameControl.Tick += new System.EventHandler(this.GameControl_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Kačer.png");
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 534);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(856, 557);
+            this.Controls.Add(this.Info);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label Info;
+        private System.Windows.Forms.Timer GameControl;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
--- zakladkachny/Form1.cs
+++ zakladkachny/Form1.cs
@@ -14,29 +14,94 @@ namespace zakladkachny
     {
 
         Button prvni;
+        Random nahoda = new Random();
+        List<Button> kachny = new List<Button>();
+        List<Button> kLikvidaci = new List<Button>();
 
-        int pocetTicku = 0;
+        int zivoty = 10;
+        int skore = 0;
 
         public Form1()
         {
             InitializeComponent();
-            prvni = new Button();
-            prvni.Text = "Kachna";
-            prvni.Left = 100;
-            prvni.Top = 10;
-            prvni.Click += this.Clicked;
-            this.Controls.Add(prvni);
+
+            for (int i = 0; i < 10; i++)
+            {
+                vytvorKachnu();
+            }
+            
+          
+            //prvni = new Button();
+            //prvni.Text = "Kachna";
+            //prvni.Left = 100;
+            //prvni.Top = 0;
+            //prvni.Click += this.Clicked;
+            //this.Controls.Add(prvni);
+            //kachny.Add(prvni);
+        }
+
+        public void vytvorKachnu()
+        {
+            Button novaKachna = new Button();
+            kachny.Add(novaKachna);
+            novaKachna.Left = nahoda.Next(Size.Width);
+            novaKachna.Top = 0;
+            novaKachna.Click += Clicked;
+            novaKachna.BackgroundImage = imageList1.Images[0];
+            novaKachna.BackgroundImageLayout = ImageLayout.Stretch;
+            novaKachna.Size = new Size(79, 49);
+            Controls.Add(novaKachna);
+                
         }
 
         public void Clicked(object sender, EventArgs args)
         {
-            prvni.Text = "Kliknuto";
-            timer.Enabled = true;
+            skore++;
+            Info.Text= "" + skore;
+            kachny.Remove((Button)sender);
+            Controls.Remove((Button)sender);
+            vytvorKachnu();
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            prvni.Text = "Tick " + pocetTicku++;
+            
+            foreach (Button b in kachny)
+            {
+                b.Top = b.Top + 5;                
+                
+                if (b.Bottom > Size.Height)
+                {
+                    skore--;
+                    Info.Text = "" + skore;
+                    kLikvidaci.Add(b);
+                }
+            }
+            foreach (Button a in kLikvidaci)
+            {
+                kachny.Remove(a);
+                Controls.Remove(a);               
+                vytvorKachnu();
+            }
+            kLikvidaci.Clear();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        struct padajciVec
+        {
+            public Button tlacitko;
+            public Timer casovac;
+        }
+
+        private void GameControl_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
